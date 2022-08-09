c223ede8ac4dbf8bdeddcd9386456422f91f1b93
Hoon Kim <mail@hoonkim.me>
2022-08-09 16:36:01 +0100

    add search config

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,40 +1,40 @@
 // @ts-check
 // Note: type annotations allow type checking and IDEs autocompletion
 
-const lightCodeTheme = require('prism-react-renderer/themes/github');
-const darkCodeTheme = require('prism-react-renderer/themes/dracula');
+const lightCodeTheme = require("prism-react-renderer/themes/github");
+const darkCodeTheme = require("prism-react-renderer/themes/dracula");
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'Astar Docs',
-  tagline: 'Your one-stop shop for everything Astar',
-  url: 'https://docs.astar.network',
-  baseUrl: '/',
-  onBrokenLinks: 'throw',
-  onBrokenMarkdownLinks: 'warn',
-  favicon: 'img/fav.png',
-  organizationName: 'AstarNetwork', // Usually your GitHub org/user name.
-  projectName: 'astar-docs', // Usually your repo name.
-  plugins: ['docusaurus-plugin-sass'],
+  title: "Astar Docs",
+  tagline: "Your one-stop shop for everything Astar",
+  url: "https://docs.astar.network",
+  baseUrl: "/",
+  onBrokenLinks: "throw",
+  onBrokenMarkdownLinks: "warn",
+  favicon: "img/fav.png",
+  organizationName: "AstarNetwork", // Usually your GitHub org/user name.
+  projectName: "astar-docs", // Usually your repo name.
+  plugins: ["docusaurus-plugin-sass"],
   stylesheets: [],
 
   presets: [
     [
-      'classic',
+      "classic",
       /** @type {import('@docusaurus/preset-classic').Options} */
       ({
         docs: {
-          sidebarPath: require.resolve('./sidebars.js'),
+          sidebarPath: require.resolve("./sidebars.js"),
           // Please change this to your repo.
-          editUrl: 'https://github.com/AstarNetwork/astar-docs/tree/main/',
+          editUrl: "https://github.com/AstarNetwork/astar-docs/tree/main/",
         },
         blog: {
           showReadingTime: true,
           // Please change this to your repo.
-          editUrl: 'https://github.com/AstarNetwork/astar-docs/tree/main/',
+          editUrl: "https://github.com/AstarNetwork/astar-docs/tree/main/",
         },
         theme: {
-          customCss: require.resolve('./src/css/custom.scss'),
+          customCss: require.resolve("./src/css/custom.scss"),
         },
       }),
     ],
@@ -44,65 +44,65 @@ const config = {
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
       navbar: {
-        title: 'Astar Docs',
+        title: "Astar Docs",
         logo: {
-          alt: 'Astar',
-          src: 'img/astar-logo.svg',
+          alt: "Astar",
+          src: "img/astar-logo.svg",
         },
         items: [
           {
-            type: 'doc',
-            docId: 'getting-started',
-            position: 'left',
-            label: 'Docs',
+            type: "doc",
+            docId: "getting-started",
+            position: "left",
+            label: "Docs",
           },
           {
-            to: 'https://medium.com/astar-network',
-            label: 'Medium',
-            position: 'left',
+            to: "https://medium.com/astar-network",
+            label: "Medium",
+            position: "left",
           },
           {
-            href: 'https://github.com/AstarNetwork/astar-docs',
-            label: 'GitHub',
-            position: 'right',
+            href: "https://github.com/AstarNetwork/astar-docs",
+            label: "GitHub",
+            position: "right",
           },
         ],
       },
       footer: {
-        style: 'light',
+        style: "light",
         links: [
           {
-            title: 'Docs',
+            title: "Docs",
             items: [
               {
-                label: 'Get Started',
-                to: '/docs/getting-started',
+                label: "Get Started",
+                to: "/docs/getting-started",
               },
             ],
           },
           {
-            title: 'Community',
+            title: "Community",
             items: [
               {
-                label: 'Discord',
-                href: 'https://discord.gg/mWGt9bH59s',
+                label: "Discord",
+                href: "https://discord.gg/mWGt9bH59s",
               },
               {
-                label: 'Twitter',
-                href: 'https://twitter.com/AstarNetwork',
+                label: "Twitter",
+                href: "https://twitter.com/AstarNetwork",
               },
             ],
           },
           {
-            title: 'More',
+            title: "More",
             items: [
               {
-                label: 'Blog',
-                to: 'https://medium.com/astar-network',
+                label: "Blog",
+                to: "https://medium.com/astar-network",
               },
               {
-                label: 'GitHub',
-                href: 'https://github.com/AstarNetwork',
+                label: "GitHub",
+                href: "https://github.com/AstarNetwork",
               },
             ],
           },
@@ -112,6 +112,29 @@ const config = {
       prism: {
         theme: lightCodeTheme,
         darkTheme: darkCodeTheme,
+      },
+      algolia: {
+        // The application ID provided by Algolia
+        appId: "S7S4T6Q4KC",
+
+        // Public API key: it is safe to commit it
+        apiKey: "4eacb78946fd33fdd34c5954c4658a7b",
+
+        indexName: "astar",
+
+        // Optional: see doc section below
+        contextualSearch: true,
+
+        // Optional: Specify domains where the navigation should occur through window.location instead on history.push. Useful when our Algolia config crawls multiple documentation sites and we want to navigate with window.location.href to them.
+        externalUrlRegex: "astar\\.network",
+
+        // Optional: Algolia search parameters
+        searchParameters: {},
+
+        // Optional: path for search page that enabled by default (`false` to disable it)
+        searchPagePath: "search",
+
+        //... other Algolia params
       },
     }),
 };
