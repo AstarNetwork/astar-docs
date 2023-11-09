d491350a0100d627d3989878cb14a36fad1e53f3
Megan Skye <58894568+meganskye@users.noreply.github.com>
2023-11-09 14:21:55 -0800

    Update figure.tsx (#509)
    this update introduces line breaks before and after images

--- src/components/figure.tsx
+++ src/components/figure.tsx
@@ -4,10 +4,12 @@ import useBaseUrl from "@docusaurus/useBaseUrl";
 export default function Figure({src, caption, width="460px"}) {
   return (
     <center>
+      <br />
       <figure>
         <img src={src} alt={caption} width={width}></img>
         <figcaption>{caption}</figcaption>
       </figure>
+      <br />
     </center>
   )
 }
