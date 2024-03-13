6b03f286b0f2f71c847aab3d7ec954df07546730
Tomasz Waszczyk <tomasz@waszczyk.com>
2024-03-13 18:31:20 +0100

    [WIP] Polish translation (#610)
    * add(bootstrap): begin of translation to Polish language
    
    * Update endpoints.md (#609)
    
    Remove pinknode from RPC providers
    
    * Update index.md - note that we are live on Shiden and Astar (#606)
    
    * init functioning version
    
    * test
    
    * updates to the endpoints available (#607)
    
    * add(pl): front page of documentation
    
    * Mainnet basic data (#611)
    
    * mainnet basic data
    
    * Update bridge-to-zkevm.md
    
    generalize to any wallet
    
    * Update bridge-to-zkevm.md
    
    ---------
    
    Co-authored-by: Mar.io <mario@stake.co.jp>
    Co-authored-by: Ivan <95318860+andabak@users.noreply.github.com>
    
    * update learn landing page and faq (#613)
    
    * Update quickstart.md (#615)
    
    * Add/local cdk node (#614)
    
    * Create setup-local-validium-node.md
    
    * Rename setup-local-node.md to setup-local-rollup-node.md
    
    * Update setup-local-rollup-node.md
    
    * Pics via upload
    
    * Additzion to setup-local-validium-node.md
    
    * Fix brackets
    
    * Fix brackets 2
    
    * Some.
    
    * Additional updates
    
    * Final updates
    
    * Final update rollup
    
    * Update headline
    
    * Post replica1
    
    * Post replica 2
    
    * Doc typos fix & Jp doc update (#617)
    
    * Typo fix & Jp doc update
    
    * Fix errors
    
    * Add Astar ZK RPC node page (#618)
    
    * Add Astar ZK RPC node page
    
    * minor rephrasing
    
    ---------
    
    Co-authored-by: Megan Skye Phoenix <58894568+meganskye@users.noreply.github.com>
    
    * Update (#619)
    
    - Improved DS registration page for devs (Polkadot.js, Talisman, Subwallet support)
    - Fixed Jp translation files
    
    * add(polish): test of build
    
    * add(pl): Czym jest Astar zkEVM?
    
    * add(formatting): white spaces
    
    * Mainnet basic data (#611)
    
    * mainnet basic data
    
    * Update bridge-to-zkevm.md
    
    generalize to any wallet
    
    * Update bridge-to-zkevm.md
    
    ---------
    
    Co-authored-by: Mar.io <mario@stake.co.jp>
    Co-authored-by: Ivan <95318860+andabak@users.noreply.github.com>
    
    * Doc typos fix & Jp doc update (#617)
    
    * Typo fix & Jp doc update
    
    * Fix errors
    
    * Update (#619)
    
    - Improved DS registration page for devs (Polkadot.js, Talisman, Subwallet support)
    - Fixed Jp translation files
    
    * add(fix): navbar
    
    * fix algolia
    
    ---------
    
    Co-authored-by: Josip Š <106158728+josip-simunovic88@users.noreply.github.com>
    Co-authored-by: Ivan <95318860+andabak@users.noreply.github.com>
    Co-authored-by: Gregory Luneau <greg@astar.network>
    Co-authored-by: Mar.io <34627453+Maar-io@users.noreply.github.com>
    Co-authored-by: Mar.io <mario@stake.co.jp>
    Co-authored-by: Megan Skye <58894568+meganskye@users.noreply.github.com>
    Co-authored-by: Marijan <77665447+mc2eqe@users.noreply.github.com>
    Co-authored-by: Gaius_sama <85451570+Gunit2481@users.noreply.github.com>
    Co-authored-by: bLd759 <18489502+bLd75@users.noreply.github.com>

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,57 +1,57 @@
 // @ts-nocheck
 // Note: type annotations allow type checking and IDEs autocompletion
-const {themes} = require('prism-react-renderer');
+const { themes } = require("prism-react-renderer");
 const lightCodeTheme = themes.github;
 const darkCodeTheme = themes.dracula;
-const math = require('remark-math');
-const katex = require('rehype-katex');
+const math = require("remark-math");
+const katex = require("rehype-katex");
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'Astar Docs',
-  tagline: 'A comprehensive library of useful information about Astar Network',
-  url: 'https://docs.astar.network',
-  baseUrl: '/',
-  onBrokenLinks: 'throw',
-  onBrokenMarkdownLinks: 'throw',
-  favicon: 'img/fav.png',
-  organizationName: 'AstarNetwork', // Usually your GitHub org/user name.
-  projectName: 'astar-docs', // Usually your repo name.
-  plugins: ['docusaurus-plugin-sass'],
+  title: "Astar Docs",
+  tagline: "A comprehensive library of useful information about Astar Network",
+  url: "https://docs.astar.network",
+  baseUrl: "/",
+  onBrokenLinks: "throw",
+  onBrokenMarkdownLinks: "throw",
+  favicon: "img/fav.png",
+  organizationName: "AstarNetwork", // Usually your GitHub org/user name.
+  projectName: "astar-docs", // Usually your repo name.
+  plugins: ["docusaurus-plugin-sass"],
   markdown: {
     mermaid: true,
   },
-  themes: ['@docusaurus/theme-mermaid'],
+  themes: ["@docusaurus/theme-mermaid"],
   stylesheets: [
     {
-      href: 'https://cdn.jsdelivr.net/npm/katex@0.13.24/dist/katex.min.css',
-      type: 'text/css',
+      href: "https://cdn.jsdelivr.net/npm/katex@0.13.24/dist/katex.min.css",
+      type: "text/css",
       integrity:
-        'sha384-odtC+0UGzzFL/6PNoE8rX/SPcQDXBJ+uRepguP4QkPCm2LBxH3FA3y+fKSiJ+AmM',
-      crossorigin: 'anonymous',
+        "sha384-odtC+0UGzzFL/6PNoE8rX/SPcQDXBJ+uRepguP4QkPCm2LBxH3FA3y+fKSiJ+AmM",
+      crossorigin: "anonymous",
     },
   ],
-  clientModules: [require.resolve('./static/astarAi.js')],
+  clientModules: [require.resolve("./static/astarAi.js")],
 
   presets: [
     [
-      'classic',
+      "classic",
       /** @type {import('@docusaurus/preset-classic').Options} */
       ({
         docs: {
-          sidebarPath: require.resolve('./sidebars.js'),
+          sidebarPath: require.resolve("./sidebars.js"),
           // Please change this to your repo.
-          editUrl: 'https://github.com/AstarNetwork/astar-docs/tree/main/',
+          editUrl: "https://github.com/AstarNetwork/astar-docs/tree/main/",
           remarkPlugins: [math],
           rehypePlugins: [katex],
         },
         blog: {
           showReadingTime: true,
           // Please change this to your repo.
-          editUrl: 'https://github.com/AstarNetwork/astar-docs/tree/main/',
+          editUrl: "https://github.com/AstarNetwork/astar-docs/tree/main/",
         },
         theme: {
-          customCss: require.resolve('./src/css/custom.scss'),
+          customCss: require.resolve("./src/css/custom.scss"),
         },
       }),
     ],
@@ -59,98 +59,98 @@ const config = {
 
   //Enable multilanguage support. Portuguese added as first language
   i18n: {
-    defaultLocale: 'en',
-    locales: ['en', "ja"],
+    defaultLocale: "en",
+    locales: ["en", "ja", "pl"],
     localeConfigs: {
-      en: { htmlLang: 'en-US' },
+      en: { htmlLang: "en-US" },
       ja: {},
-
+      pl: {},
     },
   },
   themeConfig:
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
       navbar: {
-        title: 'Astar Docs',
+        title: "Astar Docs",
         logo: {
-          alt: 'Astar',
-          src: 'img/astar-logo.svg',
+          alt: "Astar",
+          src: "img/astar-logo.svg",
         },
         items: [
           {
-            type: 'doc',
-            docId: 'getting-started',
-            position: 'left',
-            label: 'Docs',
+            type: "doc",
+            docId: "getting-started",
+            position: "left",
+            label: "Docs",
           },
           {
-            type: 'doc',
-            docId: 'build/index',
-            position: 'left',
-            label: 'Start Building',
+            type: "doc",
+            docId: "build/index",
+            position: "left",
+            label: "Start Building",
           },
           {
-            type: 'doc',
-            docId: 'build/zkEVM/quickstart',
-            position: 'left',
-            label: 'zkEVM',
+            type: "doc",
+            docId: "build/zkEVM/quickstart",
+            position: "left",
+            label: "zkEVM",
           },
           {
-            to: 'https://medium.com/astar-network',
-            label: 'Medium',
-            position: 'left',
+            to: "https://medium.com/astar-network",
+            label: "Medium",
+            position: "left",
           },
           {
-            to: 'https://www.youtube.com/channel/UC36JgEF6gqatVSK9xlzzrvQ',
-            label: 'Youtube',
-            position: 'left',
+            to: "https://www.youtube.com/channel/UC36JgEF6gqatVSK9xlzzrvQ",
+            label: "Youtube",
+            position: "left",
           },
           {
-            type: 'localeDropdown',
-            position: 'right',
+            type: "localeDropdown",
+            position: "right",
           },
           {
-            href: 'https://github.com/AstarNetwork/astar-docs',
-            label: 'GitHub',
-            position: 'right',
+            href: "https://github.com/AstarNetwork/astar-docs",
+            label: "GitHub",
+            position: "right",
           },
         ],
       },
       footer: {
-        style: 'light',
+        style: "light",
         links: [
           {
-            title: 'Docs',
+            title: "Docs",
             items: [
               {
-                label: 'Get Started',
-                to: '/docs/getting-started',
+                label: "Get Started",
+                to: "/docs/getting-started",
               },
             ],
           },
           {
-            title: 'Community',
+            title: "Community",
             items: [
               {
-                label: 'Discord',
-                href: 'https://discord.gg/astarnetwork',
+                label: "Discord",
+                href: "https://discord.gg/astarnetwork",
               },
               {
-                label: 'Twitter',
-                href: 'https://twitter.com/AstarNetwork',
+                label: "Twitter",
+                href: "https://twitter.com/AstarNetwork",
               },
             ],
           },
           {
-            title: 'More',
+            title: "More",
             items: [
               {
-                label: 'Blog',
-                to: 'https://astar.network/blog',
+                label: "Blog",
+                to: "https://astar.network/blog",
               },
               {
-                label: 'GitHub',
-                href: 'https://github.com/AstarNetwork',
+                label: "GitHub",
+                href: "https://github.com/AstarNetwork",
               },
             ],
           },
@@ -160,28 +160,28 @@ const config = {
       prism: {
         theme: lightCodeTheme,
         darkTheme: darkCodeTheme,
-        additionalLanguages: ['rust', 'toml', 'solidity'],
+        additionalLanguages: ["rust", "toml", "solidity"],
       },
       algolia: {
         // The application ID provided by Algolia
-        appId: 'S7S4T6Q4KC',
+        appId: "S7S4T6Q4KC",
 
         // Public API key: it is safe to commit it
-        apiKey: '4eacb78946fd33fdd34c5954c4658a7b',
+        apiKey: "4eacb78946fd33fdd34c5954c4658a7b",
 
-        indexName: 'astar',
+        indexName: "astar",
 
         // Optional: see doc section below
         contextualSearch: true,
 
         // Optional: Specify domains where the navigation should occur through window.location instead on history.push. Useful when our Algolia config crawls multiple documentation sites and we want to navigate with window.location.href to them.
-        externalUrlRegex: 'astar\\.network',
+        externalUrlRegex: "astar\\.network",
 
         // Optional: Algolia search parameters
         searchParameters: {},
 
         // Optional: path for search page that enabled by default (`false` to disable it)
-        searchPagePath: 'search',
+        searchPagePath: "search",
 
         //... other Algolia params
       },
---
+++ i18n/pl/docusaurus-plugin-content-docs/current/use/index.md
@@ -0,0 +1,9 @@
+# ユーザーガイド
+
+<br/>
+
+import DocCardList from '@theme/DocCardList';
+import {useCurrentSidebarCategory} from '@docusaurus/theme-common';
+
+<DocCardList items={useCurrentSidebarCategory().items}/>
+
