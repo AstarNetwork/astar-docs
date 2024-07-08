c13caf960a019a03443fe1a99ba3f7fe67b0694b
codespool <4625220+codespool@users.noreply.github.com>
2024-07-08 19:01:56 +0800

    update footer

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -8,166 +8,236 @@ const katex = require("rehype-katex");
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-	title: "Astar Docs",
-	tagline: "A comprehensive library of useful information about Astar Network",
-	url: "https://docs.astar.network",
-	baseUrl: "/",
-	onBrokenLinks: "throw",
-	onBrokenMarkdownLinks: "throw",
-	favicon: "img/fav.png",
-	organizationName: "AstarNetwork", // Usually your GitHub org/user name.
-	projectName: "astar-docs", // Usually your repo name.
-	plugins: ["docusaurus-plugin-sass"],
-	markdown: {
-		mermaid: true,
-	},
-	themes: ["@docusaurus/theme-mermaid"],
-	stylesheets: [
-		{
-			href: "https://cdn.jsdelivr.net/npm/katex@0.13.24/dist/katex.min.css",
-			type: "text/css",
-			integrity:
-				"sha384-odtC+0UGzzFL/6PNoE8rX/SPcQDXBJ+uRepguP4QkPCm2LBxH3FA3y+fKSiJ+AmM",
-			crossorigin: "anonymous",
-		},
-	],
-	clientModules: [require.resolve("./static/astarAi.js")],
+  title: "Astar Docs",
+  tagline: "A comprehensive library of useful information about Astar Network",
+  url: "https://docs.astar.network",
+  baseUrl: "/",
+  onBrokenLinks: "throw",
+  onBrokenMarkdownLinks: "throw",
+  favicon: "img/fav.png",
+  organizationName: "AstarNetwork", // Usually your GitHub org/user name.
+  projectName: "astar-docs", // Usually your repo name.
+  plugins: ["docusaurus-plugin-sass"],
+  markdown: {
+    mermaid: true,
+  },
+  themes: ["@docusaurus/theme-mermaid"],
+  stylesheets: [
+    {
+      href: "https://cdn.jsdelivr.net/npm/katex@0.13.24/dist/katex.min.css",
+      type: "text/css",
+      integrity: "sha384-odtC+0UGzzFL/6PNoE8rX/SPcQDXBJ+uRepguP4QkPCm2LBxH3FA3y+fKSiJ+AmM",
+      crossorigin: "anonymous",
+    },
+  ],
+  clientModules: [require.resolve("./static/astarAi.js")],
 
-	presets: [
-		[
-			"classic",
-			/** @type {import('@docusaurus/preset-classic').Options} */
-			({
-				docs: {
-					sidebarPath: require.resolve("./sidebars.js"),
-					// Please change this to your repo.
-					editUrl: "https://github.com/AstarNetwork/astar-docs/tree/main/",
-					remarkPlugins: [math],
-					rehypePlugins: [katex],
-				},
-				blog: {
-					showReadingTime: true,
-					// Please change this to your repo.
-					editUrl: "https://github.com/AstarNetwork/astar-docs/tree/main/",
-				},
-				theme: {
-					customCss: require.resolve("./src/css/custom.scss"),
-				},
-			}),
-		],
-	],
+  presets: [
+    [
+      "classic",
+      /** @type {import('@docusaurus/preset-classic').Options} */
+      ({
+        docs: {
+          sidebarPath: require.resolve("./sidebars.js"),
+          // Please change this to your repo.
+          editUrl: "https://github.com/AstarNetwork/astar-docs/tree/main/",
+          remarkPlugins: [math],
+          rehypePlugins: [katex],
+        },
+        blog: {
+          showReadingTime: true,
+          // Please change this to your repo.
+          editUrl: "https://github.com/AstarNetwork/astar-docs/tree/main/",
+        },
+        theme: {
+          customCss: require.resolve("./src/css/custom.scss"),
+        },
+      }),
+    ],
+  ],
 
-	//Enable multilanguage support. Portuguese added as first language
-	i18n: {
-		defaultLocale: "en",
-		locales: ["en", "ja", "es"],
-		localeConfigs: {
-			en: { htmlLang: "en-US" },
-			ja: {},
-		},
-	},
-	themeConfig:
-		/** @type {import('@docusaurus/preset-classic').ThemeConfig} */
-		({
-			navbar: {
-				title: "Astar Docs",
-				logo: {
-					alt: "Astar",
-					src: "img/astar-logo.svg",
-				},
-				items: [
-					{
-						type: "doc",
-						docId: "getting-started",
-						position: "left",
-						label: "Docs",
-					},
-					{
-						position: "left",
-						label: "Website",
-						to: "https://astar.network",
-					},
-					{
-						position: "left",
-						label: "Portal",
-						to: "https://portal.astar.network/",
-					},
-					{
-						type: "localeDropdown",
-						position: "right",
-					},
-				],
-			},
-			footer: {
-				style: "light",
-				links: [
-					{
-						title: "Docs",
-						items: [
-							{
-								label: "Get Started",
-								to: "/docs/getting-started",
-							},
-						],
-					},
-					{
-						title: "Community",
-						items: [
-							{
-								label: "Discord",
-								href: "https://discord.gg/astarnetwork",
-							},
-							{
-								label: "Twitter",
-								href: "https://twitter.com/AstarNetwork",
-							},
-						],
-					},
-					{
-						title: "More",
-						items: [
-							{
-								label: "Blog",
-								to: "https://astar.network/blog",
-							},
-							{
-								label: "GitHub",
-								href: "https://github.com/AstarNetwork",
-							},
-						],
-					},
-				],
-				copyright: `Copyright © ${new Date().getFullYear()} Astar Developers Hub.`,
-			},
-			prism: {
-				theme: lightCodeTheme,
-				darkTheme: darkCodeTheme,
-				additionalLanguages: ["rust", "toml", "solidity"],
-			},
-			algolia: {
-				// The application ID provided by Algolia
-				appId: "S7S4T6Q4KC",
+  //Enable multilanguage support. Portuguese added as first language
+  i18n: {
+    defaultLocale: "en",
+    locales: ["en", "ja", "es"],
+    localeConfigs: {
+      en: { htmlLang: "en-US" },
+      ja: {},
+    },
+  },
+  themeConfig:
+    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+    ({
+      navbar: {
+        title: "Astar Docs",
+        logo: {
+          alt: "Astar",
+          src: "img/astar-logo.svg",
+        },
+        items: [
+          {
+            type: "doc",
+            docId: "getting-started",
+            position: "left",
+            label: "Docs",
+          },
+          {
+            position: "left",
+            label: "Website",
+            to: "https://astar.network",
+          },
+          {
+            position: "left",
+            label: "Portal",
+            to: "https://portal.astar.network/",
+          },
+          {
+            type: "localeDropdown",
+            position: "right",
+          },
+        ],
+      },
+      footer: {
+        style: "light",
+        links: [
+          {
+            title: "Solutions",
+            items: [
+              {
+                label: "Solutions",
+                href: "https://astar.network",
+              },
+              {
+                label: "Startale Labs",
+                href: "https://astar.network",
+              },
+              {
+                label: "Astar Japan Lab",
+                href: "https://astar.network",
+              },
+              {
+                label: "Astar 2.0",
+                href: "https://astar.network",
+              },
+            ],
+          },
+          {
+            title: "Use",
+            items: [
+              {
+                label: "Portal",
+                href: "https://astar.network",
+              },
+              {
+                label: "dApp Staking",
+                href: "https://astar.network",
+              },
+              {
+                label: "Ecosystem",
+                href: "https://astar.network",
+              },
+            ],
+          },
+          {
+            title: "Developer",
+            items: [
+              {
+                label: "Start Building",
+                href: "https://astar.network",
+              },
+              {
+                label: "Docs",
+                to: "https://astar.network",
+              },
+            ],
+          },
+          {
+            title: "Explore",
+            items: [
+              {
+                label: "Community",
+                href: "https://astar.network",
+              },
+              {
+                label: "Blog",
+                href: "https://astar.network",
+              },
+              {
+                label: "Become our Agent",
+                href: "https://astar.network",
+              },
+              {
+                label: "Discord Community",
+                href: "https://astar.network",
+              },
+              {
+                label: "Astar Forum",
+                href: "https://astar.network",
+              },
+            ],
+          },
+          {
+            title: "About",
+            items: [
+              {
+                label: "Brand Asset Kit",
+                href: "https://astar.network",
+              },
+              {
+                label: "Careers",
+                href: "https://astar.network",
+              },
+              {
+                label: "Contact Us",
+                href: "https://astar.network",
+              },
+            ],
+          },
+          {
+            title: "Legal",
+            items: [
+              {
+                label: "Privacy Policy",
+                href: "https://astar.network",
+              },
+              {
+                label: "Terms of Use",
+                href: "https://astar.network",
+              },
+            ],
+          },
+        ],
+        copyright: `Copyright © ${new Date().getFullYear()} Astar Developers Hub.`,
+      },
+      prism: {
+        theme: lightCodeTheme,
+        darkTheme: darkCodeTheme,
+        additionalLanguages: ["rust", "toml", "solidity"],
+      },
+      algolia: {
+        // The application ID provided by Algolia
+        appId: "S7S4T6Q4KC",
 
-				// Public API key: it is safe to commit it
-				apiKey: "daf10229cac599f39cd3de3e87f85e6d",
+        // Public API key: it is safe to commit it
+        apiKey: "daf10229cac599f39cd3de3e87f85e6d",
 
-				indexName: "astar",
+        indexName: "astar",
 
-				// Optional: see doc section below
-				contextualSearch: true,
+        // Optional: see doc section below
+        contextualSearch: true,
 
-				// Optional: Specify domains where the navigation should occur through window.location instead on history.push. Useful when our Algolia config crawls multiple documentation sites and we want to navigate with window.location.href to them.
-				externalUrlRegex: "astar\\.network",
+        // Optional: Specify domains where the navigation should occur through window.location instead on history.push. Useful when our Algolia config crawls multiple documentation sites and we want to navigate with window.location.href to them.
+        externalUrlRegex: "astar\\.network",
 
-				// Optional: Algolia search parameters
-				searchParameters: {},
+        // Optional: Algolia search parameters
+        searchParameters: {},
 
-				// Optional: path for search page that enabled by default (`false` to disable it)
-				searchPagePath: "search",
+        // Optional: path for search page that enabled by default (`false` to disable it)
+        searchPagePath: "search",
 
-				//... other Algolia params
-			},
-		}),
+        //... other Algolia params
+      },
+    }),
 };
 
 module.exports = config;
